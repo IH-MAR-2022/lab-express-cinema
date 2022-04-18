0b6ae200484310aef7c4ba2c4d7f2b9d114c2318
miketroianello <mike@Mikes-MacBook-Pro.local>
2022-04-18 15:00:14 -0400

    iterations 1-3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then(function (allMovies) {
+      console.log("Found movies", allMovies);
+      res.render("movies", { allMovies: allMovies });
+    })
+    .catch(function (err) {
+      console.log("Something went wrong", err.message);
+    });
+});
 
 module.exports = router;
